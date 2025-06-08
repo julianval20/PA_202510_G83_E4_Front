635df99759051e8f02dba24102cdbbe0ce29852a
julianval20 <jevalenciav@udistrital.edu.co>
2025-06-08 14:41:16 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import Registro from "./Components/Registro";
 import Productos from "./Components/Productos";
 import Productores from "./Components/Productores";
 import ProductorDetail from "./Components/ProductorDetail";
+import ProductoDetail from "./Components/ProductoDetail";
+import Carrito from "./Components/Carrito"; // 👈 Importa el nuevo componente
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tienda" element={<Productos />} />
-        <Route path="/productores" element={<Productores />} />
-        <Route path="/productores/:productorId" element={<ProductorDetail />} />
-      </Routes>
+   <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/tienda" element={<Productos />} />
+  <Route path="/productores" element={<Productores />} />
+  <Route path="/productores/:productorId" element={<ProductorDetail />} />
+  <Route path="/productos/:productoId" element={<ProductoDetail />} />
+  <Route path="/registro" element={<Registro />} />
+</Routes>
     </BrowserRouter>
   );
 }
